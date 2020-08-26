c24cdfa53e4492358452db3fba360ae354d0ac8a
Gentylove <50623329+gentylovePY@users.noreply.github.com>
2020-08-26 12:54:40 +0400

    Initial Commit

---
+++ firstproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstproject
+//
+//  Created by мак on 26.08.2020.
+//  Copyright © 2020 roma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
